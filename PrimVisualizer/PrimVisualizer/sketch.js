871b93ea0bd4904b85b797daac87a053382b353a
Caio <caiooliv97@gmail.com>
2020-10-02 20:12:12 -0300

    Criada Connections
    Signed-off-by: Caio <caiooliv97@gmail.com>
    Co-authored-by: Matheus <matheus.1nascimento58@gmail.com>

--- PrimVisualizer/PrimVisualizer/sketch.js
+++ PrimVisualizer/PrimVisualizer/sketch.js
@@ -1,4 +1,6 @@
 var nodes = new Array();
+var connections = new Array();
+
 
 function Node(location, index) {
   this.location = location;
@@ -7,21 +9,18 @@ function Node(location, index) {
   this.index = index;
 }
 
+function Connection(newConnectedNode,connectingNode) {
+  this.n1 = newConnectedNode;
+  this.n2 = connectingNode;
+
+  connectingNode.isLeaf = false;
+}
+
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
   createNodes();
 }
 
-function drawNodes() {
-  nodes.forEach((node) => {
-    fill(0, 220, 0);
-    ellipse(node.location.x, node.location.y, 20);
-    fill(0);
-    textAlign(CENTER);
-    textSize(12);
-    text(node.index, node.location.x, node.location.y);
-  });
-}
 
 function draw() {
   background(220);
@@ -32,4 +31,15 @@ function createNodes() {
   for (let i = 0; i < 50; i++) {
     nodes.push(new Node(createVector(random(width), random(height)), i));
   }
+}
+
+function drawNodes() {
+  nodes.forEach((node) => {
+    fill(0, 220, 0);
+    ellipse(node.location.x, node.location.y, 30);
+    fill(0);
+    textAlign(CENTER);
+    textSize(12);
+    text(node.index, node.location.x, node.location.y);
+  });
 }
