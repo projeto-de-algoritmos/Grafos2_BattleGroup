76c71583a07cc9649bcfc3986a7f8e500f2d4786
mrodrigues <matheus.1nascimento58@gmail.com>
2020-10-02 22:26:43 -0300

    adicionando identificador do nó
    Signed-off-by: mrodrigues <matheus.1nascimento58@gmail.com>
    Co-authored-by: Caio <caiooliv97@gmail.com>

--- PrimVisualizer/PrimVisualizer/sketch.js
+++ PrimVisualizer/PrimVisualizer/sketch.js
@@ -1,8 +1,8 @@
-var nodes = new Array();
-var connections = new Array();
-var reached = new Array();
-var unreached = new Array();
-
+let nodes = new Array();
+let connections = new Array();
+let reached = new Array();
+let unreached = new Array();
+let visitedNodeIndex = 0;
 
 function Node(location, index) {
   this.location = location;
@@ -11,7 +11,7 @@ function Node(location, index) {
   this.index = index;
 }
 
-function Connection(newConnectedNode,connectingNode) {
+function Connection(newConnectedNode, connectingNode) {
   this.node1 = newConnectedNode;
   this.node2 = connectingNode;
 
@@ -24,9 +24,8 @@ function setup() {
   reached = [];
   unreached = nodes.slice();
 
-  prim()
+  prim();
 }
-
 
 function draw() {
   background(220);
@@ -35,95 +34,108 @@ function draw() {
 }
 
 function createNodes() {
-  console.log("WIDTH",width)
-  console.log("height",height)
-  for (let i = 0; i < 5; i++) {
+  console.log("WIDTH", width);
+  console.log("height", height);
+
+  for (let i = 0; i < random(5, 50); i++) {
     let nodeWidth = random(width) + 60;
-    if(nodeWidth > width ){
-      console.log("ENTROU AQUI")
-      nodeWidth = width/2
+    if (nodeWidth > width) {
+      console.log("ENTROU AQUI");
+      nodeWidth = width / 2;
     }
 
     let nodeHeight = random(height) + 60;
-    if(nodeHeight > height ){
-      nodeHeight = height/2
+    if (nodeHeight > height) {
+      nodeHeight = height / 2;
       console.log("ENTROU AQUI 2");
     }
 
-    console.log("Node width",nodeWidth);
-    console.log("Node height",nodeHeight);
-    nodes.push(new Node(createVector(nodeWidth, nodeHeight), i));
-    console.log
+    console.log("Node width", nodeWidth);
+    console.log("Node height", nodeHeight);
+    nodes.push(new Node(createVector(nodeWidth, nodeHeight)));
+    console.log;
   }
 }
 
 function drawNodes() {
   nodes.forEach((node) => {
-    if(node.index == 0){
-      fill(220,0,220)
-    }else{
-      fill(0, 220, 0);
-      
+    if (node.index == 1) {
+      fill(220, 0, 220);
+    } else {
+      if (node.isReached) {
+        fill(0, 220, 0);
+      }
     }
 
-      ellipse(node.location.x - 30, node.location.y - 30, 30);
-      fill(0);
-      textAlign(CENTER);
-      textSize(12);
-      text(node.index, node.location.x - 30, node.location.y -30);
+    ellipse(node.location.x - 30, node.location.y - 30, 30);
+    fill(0);
+    textAlign(CENTER);
+    textSize(12);
+    text(node.index, node.location.x - 30, node.location.y - 30);
   });
 }
 
 function drawConnections() {
-  connections.forEach(function(connection) {
+  connections.forEach(function (connection) {
     fill(0);
     stroke(0);
-    line(connection.node1.location.x -20, connection.node1.location.y -20, connection.node2.location.x -20, connection.node2.location.y -20);    
+    line(
+      connection.node1.location.x - 20,
+      connection.node1.location.y - 20,
+      connection.node2.location.x - 20,
+      connection.node2.location.y - 20
+    );
   });
 }
 
 function prim() {
- 
   connections = [];
-  nodes.forEach(function(node) {
+  nodes.forEach(function (node) {
     node.isReached = false;
     node.isLeaf = true;
   });
 
-  var firstNode = unreached[0];
+  let firstNode = unreached[0];
+  firstNode.index = visitedNodeIndex;
   firstNode.isReached = true;
   reached.push(firstNode);
 
-
+  visitedNodeIndex = visitedNodeIndex + 1;
   doCalculation();
 }
 
 function doCalculation() {
-  if(unreached.length > 0) {
+  if (unreached.length > 0) {
     calculateConnection();
-    setTimeout(doCalculation, 2000);   
+    setTimeout(doCalculation, 2000);
   } else {
     console.log("Terminou");
   }
 }
 
-
 function calculateConnection() {
-  var record = width;
-  var closestNode, connectingNode;
+  let record = width;
+  let closestNode, connectingNode;
 
-  reached.forEach(function(reachedNode) {
-    unreached.forEach(function(unreachedNode) {
-      var d = dist(reachedNode.location.x,reachedNode.location.y,unreachedNode.location.x,unreachedNode.location.y);
+  reached.forEach(function (reachedNode) {
+    unreached.forEach(function (unreachedNode) {
+      let d = dist(
+        reachedNode.location.x,
+        reachedNode.location.y,
+        unreachedNode.location.x,
+        unreachedNode.location.y
+      );
 
-      if(d < record) {
+      if (d < record) {
         record = d;
         closestNode = unreachedNode;
+        closestNode.index = visitedNodeIndex;
         connectingNode = reachedNode;
       }
     });
   });
-  connections.push(new Connection(closestNode,connectingNode));
+  visitedNodeIndex = visitedNodeIndex + 1;
+  connections.push(new Connection(closestNode, connectingNode));
   closestNode.isReached = true;
-  reached.push(unreached.splice(unreached.indexOf(closestNode),1)[0]);
+  reached.push(unreached.splice(unreached.indexOf(closestNode), 1)[0]);
 }
